1a9e092f3b530db538e13096d488c3b7494192a3
vania <137945767+dodgog@users.noreply.github.com>
2025-05-07 12:05:27 -0500

    Ide cosmetics

--- android/src/main/kotlin/Example.kt
+++ android/src/main/kotlin/Example.kt
@@ -3,7 +3,7 @@ import kotlinx.coroutines.*
 
 @Keep
 class Example {
-  public suspend fun thinkBeforeAnswering(): String {
+  suspend fun thinkBeforeAnswering(): String {
     delay(1000L)
     return "42"
   }
