e8a61f212cdf3cf4eb8c895d704de325e030b67b
vania <137945767+dodgog@users.noreply.github.com>
2025-05-21 15:59:02 -0400

    Add hlc to library and tests

--- graphplugin/android/src/main/kotlin/Example.kt
+++ graphplugin/android/src/main/kotlin/Example.kt
@@ -1,10 +1,18 @@
 import androidx.annotation.Keep
+import com.daylightcomputer.hlc.HLC
+import com.daylightcomputer.hlc.HLCConfig
+import com.daylightcomputer.hlc.HLCEnvironment
+import com.daylightcomputer.hlc.events.issueLocalEventPacked
+import com.daylightcomputer.hlc.model.DistributedNode
 import kotlinx.coroutines.*
 
 @Keep
 class Example {
-  suspend fun thinkBeforeAnswering(): String {
-    delay(1000L)
-    return "42"
-  }
+    suspend fun thinkBeforeAnswering(): String {
+        HLCEnvironment.initialize(HLCConfig())
+        val a = HLC(DistributedNode("abc123"))
+        println(a.issueLocalEventPacked())
+        delay(1000L)
+        return "42"
+    }
 }
